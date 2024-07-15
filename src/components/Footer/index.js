609b2b5896d9944fc14a5828bb96ee931b0495aa
unknown <eltonconker@gmail.com>
2024-07-15 10:26:44 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>AluraFlix &copy; Desenvolvido por Elton Maciel</h2>
+        </footer>
+    );
+    
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
+function Header() {
+    return (
+        <header className={styles.header}>
+            <img src="./images/logo.png"></img>
+            <Link to="/">
+                <span>AluraFlix</span>
+            </Link>
+            
+            <nav>
+                
+                <Link to="/">Home</Link>
+                <Link to="/search">Pesquisar</Link>
+                <Link to="/favorites">Favoritos</Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
